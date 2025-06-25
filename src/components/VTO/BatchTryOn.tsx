900c9aac146d8110352d32fcae0fbe69cd19f5be
[dyad] <fernandoguerravisualart@gmail.com>
2025-06-25 20:40:14 +0200

    Reverted all changes back to version 552577bd5b3958a6bbfee61cf09572012cbd1983

--- src/components/VTO/BatchTryOn.tsx
+++ src/components/VTO/BatchTryOn.tsx
@@ -165,8 +165,8 @@ export const BatchTryOn = () => {
     };
 
     return (
-        <div className="grid grid-cols-1 xl:grid-cols-2 gap-8 max-w-7xl mx-auto">
-            <div className="space-y-6">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+            <div className="lg:col-span-1 space-y-6">
               <Card>
                 <CardHeader><CardTitle>{t('batchMode')}</CardTitle><CardDescription>{t('chooseBatchMethod')}</CardDescription></CardHeader>
                 <CardContent>
@@ -218,7 +218,7 @@ export const BatchTryOn = () => {
                 {t('startBatchTryOn')}
               </Button>
             </div>
-            <div>
+            <div className="lg:col-span-2">
               <Card className="min-h-[75vh]">
                 <CardHeader><CardTitle>{t('batchQueue')}</CardTitle></CardHeader>
                 <CardContent>
@@ -258,4 +258,4 @@ export const BatchTryOn = () => {
             </div>
         </div>
     )
-};
+}
