ba2da693b40e77f0f83197e364dc3857e26d431c
FGVA <fernandoguerravisualart@gmail.com>
2025-07-11 13:17:44 +0200

    [dyad] Fixing incorrect import path - wrote 1 file(s)

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,7 +6,7 @@ import { useSession } from "./Auth/SessionContextProvider";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { supabase } from "@/integrations/supabase/client";
 import { Skeleton } from "./ui/skeleton";
-import { useLanguage } from "./LanguageContext";
+import { useLanguage } from "@/context/LanguageContext";
 import { useOnboardingTour } from "@/context/OnboardingTourContext";
 import { ActiveJobsTracker } from "@/components/Jobs/ActiveJobsTracker";
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
