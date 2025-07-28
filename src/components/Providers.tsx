a94969776cc65a7a2a2c26faa5454472713e818d
[dyad] <fernandoguerravisualart@gmail.com>
2025-07-29 00:21:31 +0200

    [dyad] Fix LanguageProvider context error - wrote 2 file(s)

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from "react";
 import { SessionContextProvider } from "@/components/Auth/SessionContextProvider";
 import { ThemeProvider } from "@/components/ThemeProvider";
-import { LanguageProvider } from "@/context/LanguageContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ImagePreviewProvider } from "@/context/ImagePreviewContext";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -14,23 +13,21 @@ const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <LanguageProvider>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <SessionContextProvider>
-          <QueryClientProvider client={queryClient}>
-            <ImagePreviewProvider>
-              <OnboardingTourProvider>
-                <TooltipProvider>
-                  <Toaster />
-                  <Sonner />
-                  {children}
-                  <GlobalModals />
-                </TooltipProvider>
-              </OnboardingTourProvider>
-            </ImagePreviewProvider>
-          </QueryClientProvider>
-        </SessionContextProvider>
-      </ThemeProvider>
-    </LanguageProvider>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <SessionContextProvider>
+        <QueryClientProvider client={queryClient}>
+          <ImagePreviewProvider>
+            <OnboardingTourProvider>
+              <TooltipProvider>
+                <Toaster />
+                <Sonner />
+                {children}
+                <GlobalModals />
+              </TooltipProvider>
+            </OnboardingTourProvider>
+          </ImagePreviewProvider>
+        </QueryClientProvider>
+      </SessionContextProvider>
+    </ThemeProvider>
   );
 }
