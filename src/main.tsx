9256d60397b8a77e72d073dde8137630f0a91567
[dyad] <fernandoguerravisualart@gmail.com>
2025-07-30 23:28:30 +0200

    [dyad] Fix LanguageProvider error - wrote 2 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,11 @@ import App from "./App.tsx";
 import "./globals.css";
 import { BrowserRouter } from "react-router-dom";
 import { Providers } from "./components/Providers.tsx";
-import { LanguageProvider } from "./context/LanguageContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <LanguageProvider>
-      <Providers>
-        <App />
-      </Providers>
-    </LanguageProvider>
+    <Providers>
+      <App />
+    </Providers>
   </BrowserRouter>
 );
