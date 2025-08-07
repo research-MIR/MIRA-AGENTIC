6f04f955448295d2c17bdc71a76526155c905b80
[dyad] <fernandoguerravisualart@gmail.com>
2025-08-07 23:12:16 +0200

    [dyad] Fix application provider structure - wrote 2 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import App from "./App.tsx";
 import "./globals.css";
 import { BrowserRouter } from "react-router-dom";
 import { Providers } from "./components/Providers.tsx";
+import { LanguageProvider } from "@/context/LanguageContext";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <Providers>
-      <App />
-    </Providers>
+    <LanguageProvider>
+      <Providers>
+        <App />
+      </Providers>
+    </LanguageProvider>
   </BrowserRouter>
 );
