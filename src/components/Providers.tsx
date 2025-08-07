3b4766bae7f99dd83e1d6cd2ae6bd93421d110f8
[dyad] <fernandoguerravisualart@gmail.com>
2025-08-07 20:22:33 +0200

    [dyad] Fix LanguageProvider error - wrote 2 file(s)

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -8,26 +8,29 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { OnboardingTourProvider } from "@/context/OnboardingTourContext";
 import { GlobalModals } from "./GlobalModals";
+import { LanguageProvider } from "@/context/LanguageContext";
 
 const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <SessionContextProvider>
-        <QueryClientProvider client={queryClient}>
-          <ImagePreviewProvider>
-            <OnboardingTourProvider>
-              <TooltipProvider>
-                <Toaster />
-                <Sonner />
-                {children}
-                <GlobalModals />
-              </TooltipProvider>
-            </OnboardingTourProvider>
-          </ImagePreviewProvider>
-        </QueryClientProvider>
-      </SessionContextProvider>
-    </ThemeProvider>
+    <LanguageProvider>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <SessionContextProvider>
+          <QueryClientProvider client={queryClient}>
+            <ImagePreviewProvider>
+              <OnboardingTourProvider>
+                <TooltipProvider>
+                  <Toaster />
+                  <Sonner />
+                  {children}
+                  <GlobalModals />
+                </TooltipProvider>
+              </OnboardingTourProvider>
+            </ImagePreviewProvider>
+          </QueryClientProvider>
+        </SessionContextProvider>
+      </ThemeProvider>
+    </LanguageProvider>
   );
 }
